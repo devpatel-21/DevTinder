0e40e3eec81b3b75e382454d2133820698362cb1
devpatel-21 <dev212317@gmail.com>
2026-03-10 10:35:35 +0530

    explore get, post and delete api call

--- src/app.js
+++ src/app.js
@@ -4,16 +4,36 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello Dev!");
+// If you will http://localhost:3000/, it will show "Hello Dev!" and if you will http://localhost:3000/hello, it will show Hello Dev! and if you will http://localhost:3000/test, it will show "Hello Dev!". If you will http://localhost:3000/trrtrdtdgfd write this then it will print hello dev!
+
+// But if you will change the order of the routes, for example if you will write app.use("/hello", (req, res) => { res.send("Hello hello"); }); before app.use("/", (req, res) => { res.send("Hello Dev!"); }); then it will show "Hello hello" when you will http://localhost:3000/hello and it will show "Hello Dev!" when you will http://localhost:3000/ and http://localhost:3000/test and http://localhost:3000/trrtrdtdgfd
+
+// app.use("/", (req, res) => {
+//   res.send("Hello Dev!");
+// });
+
+// app.use("/hello", (req, res) => {
+//   res.send("Hello hello");
+// });
+
+// This will match all the routes from http method call apis to /test
+// app.use("/test", (req, res) => {
+//   res.send("Hello, from server");
+// });
+
+// This will only handle get call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Dev", lastname: "Patel" });
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello");
+app.post("/user", (req, res) => {
+  // Logic to save data to database
+  res.send("Data is successfully saved to database");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello, from server");
+app.delete("/user", (req, res) => {
+  // Logic to delete data from database
+  res.send("Data is successfully deleted from database");
 });
 
 app.listen(3000, () => {
