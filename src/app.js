f330fee256f99fc8ea7e518a993a55f5ef16388e
devpatel-21 <dev212317@gmail.com>
2026-03-11 09:52:06 +0530

    Season 2 > Ep 5 > middlewares and next() function

--- src/app.js
+++ src/app.js
@@ -21,20 +21,72 @@ const app = express();
 //   res.send("Hello, from server");
 // });
 
-// This will only handle get call to /user
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Dev", lastname: "Patel" });
+// This is the example of how to handle different http method call
+// // This will only handle get call to /user
+// app.get("/user", (req, res) => {
+//   res.send({ firstname: "Dev", lastname: "Patel" });
+// });
+
+// app.post("/user", (req, res) => {
+//   // Logic to save data to database
+//   res.send("Data is successfully saved to database");
+// });
+
+// app.delete("/user", (req, res) => {
+//   // Logic to delete data from database
+//   res.send("Data is successfully deleted from database");
+// });
+
+// If we will use 'use' instead of 'get' This will match all the routes from http get method call like /abc, /abc/test, /abc/test/hello, /abc/test/hello/world
+app.get("/abc/:abcId/:name/:pass", (req, res) => {
+  console.log(req.query);
+  // http://localhost:3000/abc?user=10&name='dev'
+  console.log(req.params);
+  // http://localhost:3000/abc/110/dev/fdsa
+  res.send("This is abc route");
 });
 
-app.post("/user", (req, res) => {
-  // Logic to save data to database
-  res.send("Data is successfully saved to database");
-});
+// app.get("/ab+c", (req, res) => {
+//   res.send("This is abc route");
+// });
 
-app.delete("/user", (req, res) => {
-  // Logic to delete data from database
-  res.send("Data is successfully deleted from database");
-});
+// This is the example of how to use multiple callback function in a single route
+// In below code client will get response from first callback function we have written next() in first callback function so it will move to second callback function and print "This is second callback function" in console and it will throw error because we have already send response to client in first callback function and we can't send response again to client in second callback function. If you will remove res.send("This is second callback function"); from second callback function then it will work fine and it will print "This is second callback function" in console.
+// app.get(
+//   "/test",
+//   (req, res, next) => {
+//     res.send("This is test route");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("This is second callback function");
+//     res.send("This is second callback function");
+//   },
+// );
+
+// This will work same as above
+// app.get("/test", (req, res, next) => {
+//   console.log("This is first callback function");
+//   next();
+// });
+
+// app.get("/test", (req, res, next) => {
+//   console.log("This is second callback function");
+//   res.send("This is second callback function");
+// });
+
+// This will print "This is first callback function" in console and then it will move to second callback function and print "This is second callback function" in console but it will throw error because we have use next() in second callback function and there is not any callback function after second callback function.
+app.get(
+  "/test",
+  (req, res, next) => {
+    console.log("This is first callback function");
+    next();
+  },
+  (req, res, next) => {
+    console.log("This is second callback function");
+    next();
+  },
+);
 
 app.listen(3000, () => {
   console.log("Server is successfully running");
