d62294ee06c77fead397fdd0fa6933364db8f985
devpatel-21 <dev212317@gmail.com>
2026-03-15 11:38:18 +0530

    make dynamic code to add data from api

--- src/app2.js
+++ src/app2.js
@@ -5,18 +5,19 @@ const app2 = express();
 
 const User = require("./models/user");
 
+app2.use(express.json());
+
 app2.post("/signup", async (req, res) => {
   // Handle POST request for creating a new user
-  const userObj = {
-    firstname: "Dev",
-    lastname: "Patel",
-    email: "devpatel@example.com",
-    password: "password123",
-    age: 25,
-    gender: "Male",
-  };
-
-  const user = new User(userObj);
+  //   const userObj = {
+  //     firstname: "Dev",
+  //     lastname: "Patel",
+  //     email: "devpatel@example.com",
+  //     password: "password123",
+  //     age: 25,
+  //     gender: "Male",
+  //   };
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User is successfully created");
@@ -24,6 +25,7 @@ app2.post("/signup", async (req, res) => {
     console.log("Error is:", err.message);
     res.status(500).send("Something went wrong");
   }
+  //   console.log(req.body);
 });
 
 // We should always connect database before starting server because if we will start server before connecting database then if there is any error in connecting database then our server will be running but it will not be able to perform any database operation and it will throw error when we will try to perform any database operation. So it's always better to connect database first and then start server.
